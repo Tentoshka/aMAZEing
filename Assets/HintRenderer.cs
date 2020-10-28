1d10e87799d74fc6713a530b81054d8d72ece5de
Ваше Имя <you@example.com>
2020-10-29 04:40:06 +0700

    init

---
+++ Assets/HintRenderer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HintRenderer : MonoBehaviour
+{
+    public MazeSpawner MazeSpawner;
+
+    private LineRenderer componentLineRenderer;
+
+    private void Start()
+    {
+        componentLineRenderer = GetComponent<LineRenderer>();
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            transform.position = new Vector3(5, 1, 5);
+        } else {
+            transform.position = new Vector3(5, -1, 5);
+        }
+    }
+
+    public void DrawPath()
+    {
+            Maze maze = MazeSpawner.maze;
+            int x = maze.finishPosition.x;
+            int y = maze.finishPosition.y;
+            List<Vector3> positions = new List<Vector3>();
+
+            while ((x != 0 || y != 0) && positions.Count < 10000)
+            {
+                positions.Add(new Vector3(x * MazeSpawner.CellSize.x, y * MazeSpawner.CellSize.y, y * MazeSpawner.CellSize.z));
+
+                MazeGeneratorCell currentCell = maze.cells[x, y];
+
+                if (x > 0 &&
+                    !currentCell.WallLeft &&
+                    maze.cells[x - 1, y].DistanceFromStart < currentCell.DistanceFromStart)
+                {
+                    x--;
+                }
+                else if (y > 0 &&
+                    !currentCell.WallBottom &&
+                    maze.cells[x, y - 1].DistanceFromStart < currentCell.DistanceFromStart)
+                {
+                    y--;
+                }
+                else if (x < maze.cells.GetLength(0) - 1 &&
+                    !maze.cells[x + 1, y].WallLeft &&
+                    maze.cells[x + 1, y].DistanceFromStart < currentCell.DistanceFromStart)
+                {
+                    x++;
+                }
+                else if (y < maze.cells.GetLength(1) - 1 &&
+                    !maze.cells[x, y + 1].WallBottom &&
+                    maze.cells[x, y + 1].DistanceFromStart < currentCell.DistanceFromStart)
+                {
+                    y++;
+                }
+            }
+
+            positions.Add(Vector3.zero);
+            componentLineRenderer.positionCount = positions.Count;
+            componentLineRenderer.SetPositions(positions.ToArray());
+    }
+}
---
+++ Assets/PlayerControls3D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerControls3D : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 4;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+       float x = Input.GetAxis("Horizontal");
+       float z = Input.GetAxis("Vertical");
+       
+       Vector3 move = transform.right * x + transform.forward * z;
+       
+       controller.Move(move * speed * Time.deltaTime);
+    }
+}
