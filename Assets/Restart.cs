567ee133be3370fc39afff275c11b9f564cf6d67
Ваше Имя <you@example.com>
2020-11-27 03:59:27 +0700

    Adding finish and restart; Box view bug fix

---
+++ Assets/Restart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
